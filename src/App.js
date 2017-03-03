571a3b748bd75687a63bb50b3bb6c7311a92f956
timebox-designs <timebox.designs@gmail.com>
2017-03-03 11:56:41 -0700

    Initial checkin

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {Grid} from 'react-bootstrap';
+
+class App extends Component {
+    render() {
+        return (
+            <Grid className="splash">
+                <div className="hero">
+                    <h1>Timebox Designs</h1>
+                    <p>
+                        A consectetur himenaeos molestie nisi a a euismod varius inceptos libero non ornare nunc rhoncus
+                        suspendisse nisi a.A vestibulum vulputate mi dapibus adipiscing ut vehicula a neque proin at
+                    </p>
+                    <div className="ship"></div>
+                </div>
+            </Grid>
+        );
+    }
+}
+
+export default App;
