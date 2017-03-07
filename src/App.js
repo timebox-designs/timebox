c72a84993d025e2e69216bd66e6f8771921f80c0
Timebox Designs <timebox.designs@gmail.com>
2017-03-06 20:12:18 -0700

    Responsive layout structural changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React, {Component} from 'react';
-import {Grid} from 'react-bootstrap';
+import {Grid, Row, Col} from 'react-bootstrap';
 
 class App extends Component {
     render() {
         return (
-            <Grid className="splash">
-                <div className="hero">
-                    <h1>Timebox Designs</h1>
-                    <p>
-                        A consectetur himenaeos molestie nisi a a euismod varius inceptos libero non ornare nunc rhoncus
-                        suspendisse nisi a.A vestibulum vulputate mi dapibus adipiscing ut vehicula a neque proin at
-                    </p>
-                    <div className="ship"></div>
-                </div>
-            </Grid>
+            <div>
+                <section className="splash-container">
+                    <Grid className="splash">
+                        <div className="hero">
+                            <h1>Timebox Designs</h1>
+                            <p>
+                                A consectetur himenaeos molestie nisi a a euismod varius inceptos libero non ornare nunc
+                                rhoncus suspendisse nisi a.A vestibulum vulputate mi dapibus adipiscing ut vehicula a
+                                neque proin at
+                            </p>
+                        </div>
+                        <div className="ship"></div>
+                    </Grid>
+                </section>
+
+                <section>
+                    <Grid>
+                        <Row>
+                            <Col sm={12}>
+                                A consectetur himenaeos molestie nisi a a euismod varius inceptos libero non ornare nunc
+                                rhoncus suspendisse nisi a.A vestibulum vulputate mi dapibus adipiscing ut vehicula a
+                                neque proin at
+                            </Col>
+                        </Row>
+                    </Grid>
+                </section>
+            </div>
         );
     }
 }
